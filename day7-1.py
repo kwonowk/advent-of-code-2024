d55f6bc048d39bff19c73dbdb6696e49854f40d3
Insung Kwon <insung.kwon@sciencespo.fr>
2024-12-09 14:41:35 +0100

    Day 7 Part 1 still incomplete. Trying another method

--- day7-1.py
+++ day7-1.py
@@ -5,13 +5,13 @@ true_vals = []
 
 for line in lines:
     test_val = line[0]
-    for num in reversed(line[2:]):
+    for num in reversed(line[1:]):
         if test_val % num == 0:
             test_val = test_val // num
         else:
             test_val -= num
 
-    if test_val == line[1]:
+    if test_val :
 
         print(line)
         print('yes')
