6cca703afdeae97567744c52f22aa3585e5f3380
Insung Kwon <insung.kwon@sciencespo.fr>
2024-12-09 15:26:58 +0100

    Day 7 Part 1 done

--- day7-1.py
+++ day7-1.py
@@ -1,20 +1,28 @@
+# Read the input
 with open ('day7_input.txt', 'r') as f:
     lines = [list(map(int, line.replace(':','').split())) for line in f]
 
-true_vals = []
+true_vals = []  # To store true values
 
 for line in lines:
-    test_val = line[0]
-    for num in reversed(line[1:]):
-        if test_val % num == 0:
-            test_val = test_val // num
-        else:
-            test_val -= num
-
-    if test_val :
-
-        print(line)
-        print('yes')
-        true_vals.append(line[0])
+    numbers = line[1:]
+    loop_vals = [line[0]]   # To store results of reverse calculation
+    print(line)
+    # Reversee calculation, from right to left to check whether one of the last remaining value is the same as the first value of the list
+    for num in reversed(line[2:]):
+        temp = []
+        for l in loop_vals:
+            # If remainder is not 0, only subtraction is possible. If 0, two possiblities still stand
+            if l % num == 0:
+                temp.append(l / num)
+                temp.append(l - num)
+            else:
+                temp.append(l - num)
+        loop_vals = temp
+    for v in loop_vals:
+        if v == line[1]:
+            print(line)
+            true_vals.append(line[0])
+            break
 
 print(sum(true_vals))
